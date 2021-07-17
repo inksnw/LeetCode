7b27278f4476e547ca21ad1b58c8198b20a3db95
inksnw <inksnw@gmail.com>
2021-07-17 15:24:52 +0800

    two sum

--- code/1. Two Sum.go
+++ code/1. Two Sum.go
@@ -2,13 +2,12 @@ package leetcode
 
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
-	for n, v := range nums {
+	for index, v := range nums {
 		another := target - v
 		if _, ok := m[another]; ok {
-			return []int{m[another], n}
+			return []int{m[another], index}
 		}
-		m[v] = n
+		m[v] = index
 	}
 	return nil
-
 }
